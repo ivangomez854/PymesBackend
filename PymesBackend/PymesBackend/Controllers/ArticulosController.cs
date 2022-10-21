3b6e40f79366e03de91f403e9d13a8b54e9bd972
Ivan Gomez <ivangomez854@gmail.com>
2022-10-21 20:11:27 -0300

    paso a paso backend hasta test navegador

---
+++ PymesBackend/PymesBackend/Controllers/ArticulosController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Datos;
+
+namespace PymesBackend.Controllers
+{
+    public class ArticulosController : ApiController
+    {
+        private PymesEntities db = new PymesEntities();
+
+        // GET: api/Articulos
+        public IQueryable<Articulos> GetArticulos()
+        {
+            return db.Articulos;
+        }
+
+        // GET: api/Articulos/5
+        [ResponseType(typeof(Articulos))]
+        public IHttpActionResult GetArticulos(int id)
+        {
+            Articulos articulos = db.Articulos.Find(id);
+            if (articulos == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(articulos);
+        }
+
+        // PUT: api/Articulos/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutArticulos(int id, Articulos articulos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != articulos.IdArticulo)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(articulos).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticulosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Articulos
+        [ResponseType(typeof(Articulos))]
+        public IHttpActionResult PostArticulos(Articulos articulos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Articulos.Add(articulos);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = articulos.IdArticulo }, articulos);
+        }
+
+        // DELETE: api/Articulos/5
+        [ResponseType(typeof(Articulos))]
+        public IHttpActionResult DeleteArticulos(int id)
+        {
+            Articulos articulos = db.Articulos.Find(id);
+            if (articulos == null)
+            {
+                return NotFound();
+            }
+
+            db.Articulos.Remove(articulos);
+            db.SaveChanges();
+
+            return Ok(articulos);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ArticulosExists(int id)
+        {
+            return db.Articulos.Count(e => e.IdArticulo == id) > 0;
+        }
+    }
+}
---
+++ PymesBackend/PymesBackend/Controllers/ArticulosFamiliasController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Datos;
+
+namespace PymesBackend.Controllers
+{
+    public class ArticulosFamiliasController : ApiController
+    {
+        private PymesEntities db = new PymesEntities();
+
+        // GET: api/ArticulosFamilias
+        public IQueryable<ArticulosFamilias> GetArticulosFamilias()
+        {
+            return db.ArticulosFamilias;
+        }
+
+        // GET: api/ArticulosFamilias/5
+        [ResponseType(typeof(ArticulosFamilias))]
+        public IHttpActionResult GetArticulosFamilias(int id)
+        {
+            ArticulosFamilias articulosFamilias = db.ArticulosFamilias.Find(id);
+            if (articulosFamilias == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(articulosFamilias);
+        }
+
+        // PUT: api/ArticulosFamilias/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutArticulosFamilias(int id, ArticulosFamilias articulosFamilias)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != articulosFamilias.IdArticuloFamilia)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(articulosFamilias).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticulosFamiliasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ArticulosFamilias
+        [ResponseType(typeof(ArticulosFamilias))]
+        public IHttpActionResult PostArticulosFamilias(ArticulosFamilias articulosFamilias)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ArticulosFamilias.Add(articulosFamilias);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = articulosFamilias.IdArticuloFamilia }, articulosFamilias);
+        }
+
+        // DELETE: api/ArticulosFamilias/5
+        [ResponseType(typeof(ArticulosFamilias))]
+        public IHttpActionResult DeleteArticulosFamilias(int id)
+        {
+            ArticulosFamilias articulosFamilias = db.ArticulosFamilias.Find(id);
+            if (articulosFamilias == null)
+            {
+                return NotFound();
+            }
+
+            db.ArticulosFamilias.Remove(articulosFamilias);
+            db.SaveChanges();
+
+            return Ok(articulosFamilias);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ArticulosFamiliasExists(int id)
+        {
+            return db.ArticulosFamilias.Count(e => e.IdArticuloFamilia == id) > 0;
+        }
+    }
+}
